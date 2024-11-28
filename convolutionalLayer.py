0ad2a255793d0ce2bd17a7e4438f5a97e4a26d18
yuv76 <u.val.orian@gmail.com>
2024-11-28 16:58:01 +0200

    added convolutional layer custom implementation

---
+++ convolutionalLayer.py
@@ -0,0 +1,68 @@
+from torch import nn
+import torch
+
+class DiyConvolution(nn.Module):
+    def __init__(self, in_size, out_size, kernel_size, stride=1, padding=1):
+        super().__init__()
+        self.k_size = kernel_size
+        self.in_channels = in_size
+        self.out_channels = out_size
+        self.stride = stride
+        self.padding = padding
+
+        # define the weight
+        weights = torch.tensor(self.k_size*self.k_size*self.in_channels*self.out_channels)
+        weights = weights.view(self.in_channels, self.out_channels, self.kernel_size, self.kernel_size)
+        self.weights = nn.Parameter(weights)
+        torch.nn.init.normal_(tensor, mean=0.0, std=1.0)
+
+        # define the bias
+        bias = torch.tensor(self.out_channels)
+        self.bias = nn.Parameter(bias)
+        torch.nn.init.normal_(tensor, mean=0.0, std=1.0)
+
+    def forward(self, tensor):
+        """
+        padded_tensor = F.pad(tensor, (self.padding, self.padding, self.padding, self.padding))
+
+        batch_size, channels, height, width = padded_tensor.shape
+
+        output_height = (height - self.kernel_size) // self.my_stride + 1
+        output_width = (width - self.kernel_size) // self.my_stride + 1
+
+        output_tensor = torch.zeros(batch_size, channels, output_height, output_width, device=tensor.device)
+
+        for batch_ind in range(batch_size):
+                for out_channel_ind in range(self.out_channels):
+                    for i in range(height):
+                        for j in range(width):
+                            start_from_i = i * self.my_stride
+                            start_from_j = j * self.my_stride
+                            end_i = start_from_i + self.kernel_size
+                            end_j = start_from_j + self.kernel_size
+
+                            patch = padded_tensor[batch_ind, :, start_from_i:end_i, start_from_j:end_j]
+                            output[batch_ind, out_channel_ind, i, j] = torch.sum(
+                                patch * self.weights[out_channel_ind, :, :, :]
+                                    ) + self.bias[out_channel_ind]
+        """
+
+        # Pad tensor with zeros
+        padded_tensor = F.pad(tensor, (self.padding, self.padding, self.padding, self.padding))
+
+        batch_size, channels, height, width = padded_tensor.shape
+
+        output_height = (height - self.kernel_size) // self.my_stride + 1
+        output_width = (width - self.kernel_size) // self.my_stride + 1
+
+        #output_tensor = torch.zeros(batch_size, channels, output_height, output_width, device=tensor.device)
+
+        # separate the tensor to patches in the size of the kernel, dims batch_size x (channels * kernel_size * kernel_size) x (output_height * output_width)
+        unfolded_tensor = torch.nn.functional.unfold(tensor, kernel_size=(self.kernel_size, self.kernel_size), stride=self.my_stride)
+        unfolded = unfolded_tensor.view(batch_size, channels, self.kernel_size*self.kernel_size, -1)
+
+        output_tensor = torch.sum(unfolded * self.weights) + self.bias
+
+        output_tensor = output_tensor.view(batch_size, channels, output_height, output_width)
+
+        return output_tensor
--- diyMaxPooling.py
+++ diyMaxPooling.py
@@ -38,13 +38,13 @@ class DiyMaxPooling(nn.Module):
         output_height = (height - self.kernel_size) // self.my_stride + 1
         output_width = (width - self.kernel_size) // self.my_stride + 1
 
-        # separate the tensor to patches in the size of the kernal,  dims batch_size x (channels * kernel_size * kernel_size) x (output_height * output_width)
-        unfolded_tensor = torch.nn.functional.unfold(tensor,kernel_size=(self.kernel_size, self.kernel_size),stride=self.my_stride)
+        # separate the tensor to patches in the size of the kernel,  dims batch_size x (channels * kernel_size * kernel_size) x (output_height * output_width)
+        unfolded_tensor = torch.nn.functional.unfold(tensor, kernel_size=(self.kernel_size, self.kernel_size), stride=self.my_stride)
         # we want the dims to be: 0- batch , 1- channels, 2 actual values (kernal_size*kernal_size), -1 means put the leftover in another dim automatically, they will be 
         # The -1 will automatically compute the last dimension (output_height*output_width)
-        unfolded = unfolded_tensor.view(batch_size,channels,self.kernel_size*self.kernel_size,-1)
+        unfolded = unfolded_tensor.view(batch_size, channels, self.kernel_size*self.kernel_size, -1)
 
         # for each patch take only the max values for dim 2 (the actual values since its 0- batch , 1- channels, 2 actual values(kernal_size*kernal_size))
-        res = torch.amax(unfolded,dim=VALUES_DIM)
+        res = torch.amax(unfolded, dim=VALUES_DIM)
         # shape it to be one uniform tensor (not in patches)
-        return res.view(batch_size,channels,output_height,output_width)
+        return res.view(batch_size, channels, output_height, output_width)
