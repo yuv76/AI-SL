6e3edc1a36e36cbecd13dde93829bea051912eac
Bar weiss <barboor03@gmail.com>
2024-12-08 12:35:25 +0000

    Added data augmentation, problem-validation is also augmented will get fixed soon

--- TrainingModelPytorchCustomLayers.py
+++ TrainingModelPytorchCustomLayers.py
@@ -1,67 +1,15 @@
-import glob
 import Constants
-import ModelPytorchCustomLayers as ModelPytorch
+import ModelPytorch
 import cv2
 import torch
-from torch.utils.data import Dataset, random_split
 import torch.nn.functional as F
-import torchvision.datasets as datasets
-import torchvision.transforms as transforms
 from torch import optim
 from torch import nn
 from torch.utils.data import DataLoader
 from tqdm import tqdm
+from Datasets import ASLTrainDataset,ASLTestDataset
+from torch.utils.data import random_split
 import matplotlib.pyplot as plt
-import time
-
-class ASLDataset(Dataset):
-    def __init__(self, path):
-        self.imgs_path = path
-        file_list = glob.glob(self.imgs_path + "*")
-        print(file_list)
-        self.data = []
-        self.class_map = {}
-        i = 0
-
-        # for each folder
-        for class_path in file_list:
-            class_name = class_path.split("\\")[-1]
-            self.class_map[class_name] = i
-            i += 1
-
-            # add image path and the corr class to the data
-            for img_path in glob.glob(class_path + "/*.png"):
-                self.data.append([img_path, class_name])
-        #print(self.data)
-        #print(self.class_map)
-        self.img_dim = (64, 64)
-
-    def __len__(self):
-        return len(self.data)
-
-    def __getitem__(self, idx):
-        img_path, class_name = self.data[idx]
-        # load img
-        img = cv2.imread(img_path,  cv2.IMREAD_GRAYSCALE)
-
-        # resize it to make sure it is 64*64
-        img = cv2.resize(img, self.img_dim)
-        class_id = self.class_map[class_name]
-
-
-        transform = transforms.Compose([
-            #transforms.RandomHorizontalFlip(p=0.5),  # randomly flip the image horizontally with a 50% chance
-            #transforms.RandomVerticalFlip(p=0.5),    # randomly flip the image vertically with a 50% chance
-            transforms.ToTensor(),  # Converts image to a Tensor in the range [0, 1]
-            transforms.Normalize((0.5,), (0.5,))   
-        ])
-
-        # Apply the transformation to the image
-        img_tensor = transform(img)
-
-        return img_tensor.float(), torch.tensor(class_id, dtype=torch.long)
-
-
 
 def check_accuracy(loader, model,device):
     """
@@ -97,11 +45,10 @@ def check_accuracy(loader, model,device):
     return accuracy
 
 
-def train_model(model,train_loader,valid_loader, device,optimizer,max_patiance):
+def train_model(model, train_loader, valid_loader, device, optimizer, max_patiance):
     # Will save train and validation accuracies each epoch
     train_accuracies = []
     valid_accuracies = []
-
     prev_val_acc = -1
     curr_val_acc = -1
     patience_count = 0
@@ -111,7 +58,7 @@ def train_model(model,train_loader,valid_loader, device,optimizer,max_patiance):
         if patience_count <= max_patiance:
             model.train()
             print(f"Epoch [{epoch + 1}/{Constants.NUM_EPOCHS}]")
-
+            
             for batch_index, (data, targets) in enumerate(tqdm(train_loader)):
                 # Move data and targets to the device (GPU/CPU)
                 data = data.to(device)
@@ -129,87 +76,78 @@ def train_model(model,train_loader,valid_loader, device,optimizer,max_patiance):
                 optimizer.step()
 
             print("Checking accuracy on training data")
-            train_accuracy = check_accuracy(train_loader, model,device)
+            train_accuracy = check_accuracy(train_loader, model, device)
             train_accuracies.append(train_accuracy)
+
             print("Checking accuracy on validation data")
-            curr_val_acc = check_accuracy(valid_loader, model,device)
+            curr_val_acc = check_accuracy(valid_loader, model, device)
             valid_accuracies.append(curr_val_acc)
 
             if(curr_val_acc > prev_val_acc):
                 print("Model was saved!\nPatiance reset to 0!")
-
                 # save a checkpint
-                torch.save({'epoch': epoch, 'model_state_dict': model.state_dict(),
-                            'optimizer_state_dict': optimizer.state_dict(),'loss': loss,}, Constants.PATH_CHECKPOINT)
+                torch.save({
+                    'epoch': epoch,
+                    'model_state_dict': model.state_dict(),
+                    'optimizer_state_dict': optimizer.state_dict(),
+                    'loss': loss,
+                }, Constants.PATH_CHECKPOINT)
                 patience_count = 0
             else:
+                patience_count += 1
                 print(f"Model didn't improve and therfore, was not saved!\nPatiance increased by 1 and now is : {patience_count} !")
-                patience_count +=1
 
-                print("Loading last model!")
-                checkpoint = torch.load(Constants.PATH_CHECKPOINT, weights_only=True)
-                model.load_state_dict(checkpoint['model_state_dict'])
-                optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
-                epoch = checkpoint['epoch']
-                loss = checkpoint['loss']
-
+            print("Loading last model!")
+            checkpoint = torch.load(Constants.PATH_CHECKPOINT)
+            model.load_state_dict(checkpoint['model_state_dict'])
+            optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+            epoch = checkpoint['epoch']
+            loss = checkpoint['loss']
             prev_val_acc = curr_val_acc
+
     return train_accuracies, valid_accuracies
 
-
 if __name__ == "__main__":
-    start_time = time.time()
-
-    TEST_DATA_PATH = "mydata\\test_set\\"
-    TRAIN_DATA_PATH = "mydata\\training_set\\"
-
     # use the gpu (if a gpu is present)
     device = "cuda" if torch.cuda.is_available() else "cpu"
 
     # load the train and test dataset
-    train_dataset = ASLDataset(TRAIN_DATA_PATH)
+    train_dataset = ASLTrainDataset(Constants.TRAIN_DATA_PATH)
 
     # Calculate sizes of train and valid
     total_size = len(train_dataset)
-    train_size = int (total_size * Constants.TRAIN_RATIO)
+    train_size = int(total_size * Constants.TRAIN_RATIO)
     validation_size = total_size - train_size
-
+    
     train_dataset, validation_dataset = random_split(train_dataset, [train_size, validation_size])
+    
     validation_loader = DataLoader(dataset=validation_dataset, batch_size=Constants.BATCH_SIZE, shuffle=True)
     train_loader = DataLoader(dataset=train_dataset, batch_size=Constants.BATCH_SIZE, shuffle=True)
-
-    test_dataset = ASLDataset(TEST_DATA_PATH)
+    
+    test_dataset = ASLTestDataset(Constants.TEST_DATA_PATH)
     test_loader = DataLoader(dataset=test_dataset, batch_size=Constants.BATCH_SIZE, shuffle=False)
 
-    # initialize model
+    # initialize model 
     model = ModelPytorch.CNN(in_channels=1, num_classes=Constants.NUM_CLASSES).to(device)
-
     criterion = nn.CrossEntropyLoss()
     optimizer = optim.Adam(model.parameters(), lr=Constants.LEARNING_RATE)
 
-    train_accuracies, valid_accuracies = train_model(model,train_loader,validation_loader,device,optimizer,Constants.PATIENCE)
-    end_time = time.time()
+    train_accuracies, valid_accuracies = train_model(model, train_loader, validation_loader, device, optimizer, Constants.PATIENCE)
 
     print("FINALE ACCURACY:")
     # Final accuracy check on training and test sets
-    check_accuracy(train_loader, model,device)
-    test_accuracy = check_accuracy(test_loader, model,device)
+    check_accuracy(train_loader, model, device)
+    check_accuracy(test_loader, model, device)
 
-    total_time = end_time - start_time
-
-    # After training, plot the accuracy over epochs
-    plt.plot(range(1, Constants.NUM_EPOCHS + 1), train_accuracies, label="Training Accuracy", color='b', marker='o')
-    plt.plot(range(1, Constants.NUM_EPOCHS + 1), valid_accuracies, label="Validation Accuracy", color='r', marker='x')
-    plt.scatter(Constants.NUM_EPOCHS, test_accuracy, label="Test Accuracy", color='g', s=100, zorder=5)  # s=100 controls dot size
+    # Plot training and validation accuracies
+    plt.plot(range(1, len(train_accuracies) + 1), train_accuracies, label="Training Accuracy", color='b', marker='o')
+    plt.plot(range(1, len(valid_accuracies) + 1), valid_accuracies, label="Validation Accuracy", color='r', marker='x')
     plt.xlabel('Epochs')
     plt.ylabel('Accuracy (%)')
-    plt.title(f'Training and Validation Accuracy Over Epochs\n Took total time of {total_time} seconds')
+    plt.title('Training and Validation Accuracy Over Epochs')
     plt.grid(True)
     plt.legend()
+    plt.savefig('accuracy.png')
 
-    plt.savefig('accuracy_custom.png')
-
-    # plt.show() # open the saved graph
-
-    torch.save(model.state_dict(), 'custom_model_weights.pth')
-    print("Model saved")
+    torch.save(model.state_dict(), Constants.PATH_MODEL)
+    print("Model saved") 
