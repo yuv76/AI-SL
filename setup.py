4be38208b54bf94c8244111628d58c1a33ee988e
Yuval Orian <u.val.orian@gmail.com>
2025-02-16 08:38:58 +0000

    Edit setup.py to include .pth files 🤞🤞

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ setup(
     name="ASL_detect",
     version="0.1",
     packages=find_packages(),
+    package_data={
+        'ASL_detect': ['*.pth'],
+    },
     install_requires=[],
     author="1603",
     description="A simple library that uses the CNN model we created and makes it easier to use.",
